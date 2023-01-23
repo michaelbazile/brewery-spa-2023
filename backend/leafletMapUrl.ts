4309f814f3ee10de1d4319815cadc09b8af8aea5
Michael Bazile <mr.michaelbazilejr@gmail.com>
2023-01-23 03:53:37 -0600

    feat: add intergration to load leaflet map

---
+++ backend/leafletMapUrl.ts
@@ -0,0 +1,34 @@
+import sha1 from "npm:sha1@1.1.1";
+import { v4 as uuidv4 } from "npm:uuid@9.0.0";
+import { env } from "./config.ts";
+
+export const leafletMapUrlGenerator = async (bufferURI: string) => {
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const public_id = uuidv4();
+
+  // authentication signature for interacting with cloudinary upload api
+  const cloudinaryRequestSignature = sha1(
+    `public_id=${public_id}&timestamp=${timestamp}${env.CLOUDINARY_SECRET_API_KEY}`
+  ).toString();
+
+  const body = {
+    public_id,
+    api_key: env.CLOUDINARY_API_KEY,
+    file: bufferURI,
+    signature: cloudinaryRequestSignature,
+    timestamp,
+  };
+
+  // upload map image to cloudinary's Upload API
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${env.CLOUD_NAME}/image/upload`,
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+  return response.json();
+};
